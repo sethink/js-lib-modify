df0becfef098a1dab1a13b14a26f297979afd7c4
sethink <358066237@qq.com>
2019-11-20 08:53:13 +0800

    修改myscroll支持pc端

--- aui-scroll/myScroll.js
+++ aui-scroll/myScroll.js
@@ -1,65 +1,59 @@
-(function(window) {
-	'use strict';
-	var isToBottom = false,isMoved = false;
-	var myScroll = function (params,callback) {
-		this.extend(this.params, params);
-		this._init(params,callback);
-	};
-	myScroll.prototype = {
-		params: {
-			listren:false,
+(function (window) {
+    'use strict';
+    var isToBottom = false;
+    var myScroll = function (params, callback) {
+        this.extend(this.params, params);
+        this._init(params, callback);
+    };
+    myScroll.prototype = {
+        params: {
+            listen: false,
             distance: 100,
-			dom: document.body
+            dom: document.body
         },
-		_init : function(params,callback) {
-			var self = this;
-			if(self.params.listen){
-				self.params.dom.addEventListener("touchmove", function(e){
-					self.scroll(callback);
-				});
-				self.params.dom.addEventListener("touchend", function(e){
-					self.scroll(callback);
-				});
-			}
-			window.onscroll = function(){
-				self.scroll(callback);
-			}
-		},
-		scroll : function (callback) {
-			var self = this;
+        _init: function (params, callback) {
+            var self = this;
+            if (self.params.listen) {
+                self.params.dom.addEventListener("touchmove", function (e) {
+                    self.scroll(callback);
+                });
+                self.params.dom.addEventListener("touchend", function (e) {
+                    self.scroll(callback);
+                });
 
-			var clientHeight = self.params.dom.scrollTop === 0 ? self.params.dom.clientHeight : self.params.dom.clientHeight;
-			var scrollTop = self.params.dom.scrollTop === 0 ? self.params.dom.scrollTop : self.params.dom.scrollTop;
-			var scrollHeight = self.params.dom.scrollTop === 0 ? self.params.dom.scrollHeight : self.params.dom.scrollHeight;
+                self.params.dom.addEventListener('scroll', function (ev) {
+                    self.scroll(callback);
+                });
+            }
+        },
+        scroll: function (callback) {
+            var self = this;
 
-			if (scrollHeight-scrollTop-self.params.distance <= clientHeight) {
-	        	isToBottom = true;
-	        	if(isToBottom){
-	        		callback({
-						"clientHeight":clientHeight,
-						"scrollTop":scrollTop,
-						"scrollHeight":scrollHeight,
-	        			"isToBottom":true
-	        		})
-	        	}
-	        }else{
-	        	isToBottom = false;
-	        	callback({
-					"clientHeight":clientHeight,
-					"scrollTop":scrollTop,
-					"scrollHeight":scrollHeight,
-        			"isToBottom":false
-        		})
-	        }
-		},
-        extend: function(a, b) {
-			for (var key in b) {
-			  	if (b.hasOwnProperty(key)) {
-			  		a[key] = b[key];
-			  	}
-		  	}
-		  	return a;
-		}
-	}
-	window.myScroll = myScroll;
+            var clientHeight = self.params.dom.scrollTop === 0 ? self.params.dom.clientHeight : self.params.dom.clientHeight;
+            var scrollTop = self.params.dom.scrollTop === 0 ? self.params.dom.scrollTop : self.params.dom.scrollTop;
+            var scrollHeight = self.params.dom.scrollTop === 0 ? self.params.dom.scrollHeight : self.params.dom.scrollHeight;
+
+            if (scrollHeight - scrollTop - self.params.distance <= clientHeight) {
+                isToBottom = true;
+            } else {
+                isToBottom = false;
+            }
+
+            callback({
+                "clientHeight": clientHeight,
+                "scrollTop": scrollTop,
+                "scrollHeight": scrollHeight,
+                "isToBottom": isToBottom
+            });
+        },
+        extend: function (a, b) {
+            for (var key in b) {
+                if (b.hasOwnProperty(key)) {
+                    a[key] = b[key];
+                }
+            }
+            return a;
+        }
+    };
+    window.myScroll = myScroll;
 })(window);
